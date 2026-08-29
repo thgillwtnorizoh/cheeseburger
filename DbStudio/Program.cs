1ee0346f0a8ffeb017f6bf541b1d85dab29cf34d
thgillwtnorizoh <81919671+thgillwtnorizoh@users.noreply.github.com>
2026-08-29 20:46:11 +0700

    Add DB Studio entry point

---
+++ DbStudio/Program.cs
@@ -0,0 +1,11 @@
+namespace Cheeseburger.DbStudio;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
